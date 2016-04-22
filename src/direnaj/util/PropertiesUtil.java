1b221fde295476bde697935a1648281fa5beb3de
Meddre5911 <erdem@erdem-HP-Pavilion-g6-Notebook-PC>
2016-04-22 09:01:47 +0300

    Canli gecis oncesi ayarlamalar

--- src/direnaj/util/PropertiesUtil.java
+++ src/direnaj/util/PropertiesUtil.java
@@ -9,73 +9,71 @@ import org.apache.log4j.Logger;
 
 public class PropertiesUtil {
 
-    private Properties prop;
-    private static PropertiesUtil propertiesUtil;
+	private Properties prop;
+	private static PropertiesUtil propertiesUtil;
 
-    private PropertiesUtil() {
-        prop = new Properties();
-        InputStream input = null;
-        try {
-            input = new FileInputStream("/home/erdem/git/DirenajToolkitService/propertyFiles/toolkitConfig.properties");
-//            input = new FileInputStream("/var/lib/tomcat7/toolkitConfig/toolkitConfig.properties");
-            //            input = new FileInputStream("/data/direnaj/toolkitConfig.properties");
-            // load a properties file
-            prop.load(input);
-            // get the property value and print it out
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("direnajUserId"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("direnajPassword"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("neo4j.server.rootUri"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.server.address"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.server.port"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.bulk.insert.size"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.usedDB"));
-            Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("tweet.checkInterval.inWeeks"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	private PropertiesUtil() {
+		prop = new Properties();
+		InputStream input = null;
+		try {
+			input = new FileInputStream("/home/direnaj/toolkit/toolkitConfig/toolkitConfig.properties");
+			// load a properties file
+			prop.load(input);
+			// get the property value and print it out
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("direnajUserId"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("direnajPassword"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("neo4j.server.rootUri"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.server.address"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.server.port"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.bulk.insert.size"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("mongo.usedDB"));
+			Logger.getLogger(PropertiesUtil.class).debug(prop.getProperty("tweet.checkInterval.inWeeks"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    public static PropertiesUtil getInstance() {
-        if (propertiesUtil == null) {
-            propertiesUtil = new PropertiesUtil();
-        }
-        return propertiesUtil;
-    }
+	public static PropertiesUtil getInstance() {
+		if (propertiesUtil == null) {
+			propertiesUtil = new PropertiesUtil();
+		}
+		return propertiesUtil;
+	}
 
-    public String getProperty(String property, String defaultValue) {
-        try {
-            String foundProperty = prop.getProperty(property);
-            if (TextUtils.isEmpty(foundProperty)) {
-                foundProperty = defaultValue;
-            }
-            return foundProperty;
-        } catch (Exception e) {
-            return defaultValue;
-        }
-    }
+	public String getProperty(String property, String defaultValue) {
+		try {
+			String foundProperty = prop.getProperty(property);
+			if (TextUtils.isEmpty(foundProperty)) {
+				foundProperty = defaultValue;
+			}
+			return foundProperty;
+		} catch (Exception e) {
+			return defaultValue;
+		}
+	}
 
-    public Integer getIntProperty(String property, Integer defaultValue) {
-        try {
-            return Integer.valueOf(prop.getProperty(property));
-        } catch (Exception e) {
-            return defaultValue;
-        }
-    }
-    
-    public Boolean getBooleanProperty(String property, Boolean defaultValue) {
-    	try {
-    		return Boolean.valueOf(prop.getProperty(property));
-    	} catch (Exception e) {
-    		return defaultValue;
-    	}
-    }
+	public Integer getIntProperty(String property, Integer defaultValue) {
+		try {
+			return Integer.valueOf(prop.getProperty(property));
+		} catch (Exception e) {
+			return defaultValue;
+		}
+	}
+
+	public Boolean getBooleanProperty(String property, Boolean defaultValue) {
+		try {
+			return Boolean.valueOf(prop.getProperty(property));
+		} catch (Exception e) {
+			return defaultValue;
+		}
+	}
 
 }
